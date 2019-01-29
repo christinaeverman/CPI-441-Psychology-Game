349c77737f73a8017ea3d88b1945d456df8c1e7e
Christina Everman <ceverman@asu.edu>
2019-01-29 04:15:58 -0700

    First Person Controls & Test GameObjects
    -Added and modified Mike's scripts
    -Added first person movement using WASD
    -First person camera seems to be working in both x and y directions
    -Added test GameObjects to function as clues and to use as references to test camera and player movements
    -Put placeholder clues in the inventory

---
+++ Assets/Scripts/P_Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P_Movement : MonoBehaviour
+{
+    public float speed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.localPosition += Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up) * speed;
+        if (Input.GetKey(KeyCode.S))
+            transform.localPosition -= Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up) * speed;
+        if (Input.GetKey(KeyCode.A))
+            transform.localPosition -= Vector3.ProjectOnPlane(Camera.main.transform.right, Vector3.up) * speed;
+        if (Input.GetKey(KeyCode.D))
+            transform.localPosition += Vector3.ProjectOnPlane(Camera.main.transform.right, Vector3.up) * speed;
+    }
+}
