9eeb3c6edbc9b12b2f2d7b465de1b4f199e3ccc6
Christina Everman <ceverman@asu.edu>
2019-02-05 06:08:30 -0700

    Clues
    Added locations and basic models for the clues. When the player finds a clue, it appears in the inventory.

--- Assets/Scripts/Clue_Object.cs
+++ Assets/Scripts/Clue_Object.cs
@@ -8,6 +8,7 @@ public class Clue_Object : MonoBehaviour
 		seen is what keep tracks if the clue has been been detected by the ray cast emitted by the player
 	 */
     public bool Seen = false;
+    public bool Found = false;
     /*org(orginal) stores its actual texture for when
 	it needs to change back while my is the current texture that is 
 	currently on the object itself
@@ -24,14 +25,26 @@ public class Clue_Object : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Checks if it is seen or not if it is it changes color other wise it resetss
-        if (Seen == true)
+        // highlights clue if close and hovering over it
+        if (Seen && !Found)
         {
             my_material.color = Color.yellow;
         }
-        else {
+        // if the player does not click on the clue and hovers away from it, then it has not been found yet
+        else if (!Found)
+        {
             my_material.color = Color.gray;
         }
-        Debug.Log(Seen);
+
+    }
+
+    void OnMouseDown()
+    {
+        // clue is added to the inventory
+        if (Seen == true)
+        {
+            Found = true;
+            my_material.color = Color.red;
+        }
     }
 }
---
+++ Assets/Scripts/Door_Behavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Behavior : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject panel;
+    public GameObject text;
+    private float Timer = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Timer > 0.0f && Timer < 30f)
+            Timer += 1f;
+        if (Timer > 30f)
+        {
+            panel.gameObject.SetActive(false);
+            text.gameObject.SetActive(false);
+            Timer = 0.0f;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        float distance = Vector3.Distance(player.transform.position, transform.position);
+
+        if (distance < 3f && LevelManager.FoundAll)
+        {
+            Debug.Log("Open!");
+        }
+        else if (distance < 3f && !LevelManager.FoundAll)
+        {
+            panel.gameObject.SetActive(true);
+            text.gameObject.SetActive(true);
+            Timer += 1f;
+        }
+    }
+}
