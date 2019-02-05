eb27d2dcb58061671ec2a0f0294cb1475b8cc650
Christina Everman <ceverman@asu.edu>
2019-02-05 14:58:48 -0700

    Door
    Added door functionality so that the door appears to open when the player has found all the clues.

--- Assets/Scripts/Door_Behavior.cs
+++ Assets/Scripts/Door_Behavior.cs
@@ -7,7 +7,7 @@ public class Door_Behavior : MonoBehaviour
     public GameObject player;
     public GameObject panel;
     public GameObject text;
-    private float Timer = 0.0f;
+    public GameObject button;
 
     // Start is called before the first frame update
     void Start()
@@ -18,14 +18,7 @@ public class Door_Behavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Timer > 0.0f && Timer < 30f)
-            Timer += 1f;
-        if (Timer > 30f)
-        {
-            panel.gameObject.SetActive(false);
-            text.gameObject.SetActive(false);
-            Timer = 0.0f;
-        }
+        
     }
 
     void OnMouseDown()
@@ -35,12 +28,14 @@ public class Door_Behavior : MonoBehaviour
         if (distance < 3f && LevelManager.FoundAll)
         {
             Debug.Log("Open!");
+            transform.localPosition += new Vector3(0, 0, -0.2f);
+            transform.Rotate(Vector3.up * -160);
         }
         else if (distance < 3f && !LevelManager.FoundAll)
         {
             panel.gameObject.SetActive(true);
             text.gameObject.SetActive(true);
-            Timer += 1f;
+            button.gameObject.SetActive(true);
         }
     }
 }
