35a24a9548f30bff4de7921ee5b718cbd99827b1
apurific <ampurify16@yahoo.com>
2019-02-04 23:12:08 -0700

    Hub and Door 1
    Created Hub and door 1 goes to the game room

---
+++ Assets/Scripts/LoadRoom1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadRoom1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void OnTriggerEnter(Collider coll)
+	{
+		if(coll.gameObject.tag == "MainCamera")
+			Application.LoadLevel("Game");
+	}
+}
