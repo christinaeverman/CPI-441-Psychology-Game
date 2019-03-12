1425e2c71de63f5c9b3ef819fad95027d8b1b93d
Christina Everman <ceverman@asu.edu>
2019-03-12 15:58:49 -0700

    Debugging

---
+++ Assets/Scripts/LoadRoom.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadRoom : MonoBehaviour
+{
+    public string roomName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "MainCamera")
+            Application.LoadLevel(roomName);
+    }
+}
