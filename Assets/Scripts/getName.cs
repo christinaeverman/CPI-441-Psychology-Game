11bfa23229ecfe944df0fc907ff2dde45db4c3a8
Christina Everman <ceverman@asu.edu>
2019-04-16 17:06:31 -0700

    Successfully merged our branches!

---
+++ Assets/Scripts/getName.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class getName : MonoBehaviour
+{
+	public InputField name;
+	public Text myText;
+
+	public void display()
+	{
+		myText.text = name.text + " has passed the psychology room quiz!";
+	}
+}
