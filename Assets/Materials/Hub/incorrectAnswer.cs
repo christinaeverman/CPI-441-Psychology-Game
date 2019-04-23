6be47c261646baa9455eac982ddeea483c6b2a26
Christina Everman <ceverman@asu.edu>
2019-04-23 15:31:01 -0700

    Fixed

--- Assets/Materials/Hub/incorrectAnswer.cs
+++ Assets/Materials/Hub/incorrectAnswer.cs
@@ -7,19 +7,22 @@ using UnityEngine.SceneManagement;
 public class incorrectAnswer : MonoBehaviour
 {
 	[SerializeField] private Image customImage;
+    [SerializeField] private Text customText;
     // Start is called before the first frame update
-	void  Update()
+    void  Update()
 	{
 		if((PlayerPrefs.GetInt("correctQuiz1", 0) != 1 || PlayerPrefs.GetInt("correctQuiz2", 0) != 1 ||
             PlayerPrefs.GetInt("correctQuiz3", 0) != 1 || PlayerPrefs.GetInt("correctQuiz4", 0) != 1 ||
             PlayerPrefs.GetInt("correctQuiz5", 0) != 1) && PlayerPrefs.GetInt("AnswersMoved", 0) == 5)
 		{
 			customImage.enabled = true;
+            customText.enabled = true;
 		}
 
         if (PlayerPrefs.GetInt("AnswersMoved", 0) < 5)
         {
             customImage.enabled = false;
+            customText.enabled = false;
         }
 	}
 }
